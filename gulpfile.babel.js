d82f51e678c6f4e52aa04829b2baf930ac5f3f5a
Ahmad Raza <raza.ataki@gmail.com>
2022-05-27 22:09:37 +0500

    create folders for gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,10 @@
-// const gulp = require("gulp");
-
-// gulp.task("default", defaultTask);
-
-// function defaultTask(done) {
-//   console.log("hey");
-//   done();
-// }
-
-// gulp.task("hello", function (done) {
-//   console.log("Hello!");
-//   done();
-// });
-
 import gulp from "gulp";
+import yargs from "yargs";
+
+const PRODUCTION = yargs.argv.prod;
 
 export const hello = (done) => {
-  console.log("hello");
+  console.log(PRODUCTION);
   done();
 };
 
