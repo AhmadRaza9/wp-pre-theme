c8b636ea0f9a5c891e7a2a1d44162f30b52805d2
Ahmad Raza <raza.ataki@gmail.com>
2022-05-28 19:56:22 +0500

    enqueue js for admin

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -10,6 +10,8 @@ add_action('wp_enqueue_scripts', 'firsttheme_assets', 10);
 function firsttheme_admin_assets()
 {
     wp_enqueue_style('firsttheme-admin-stylesheet', get_template_directory_uri() . '/dist/asset/css/admin.css', array(), 'all');
+    wp_enqueue_script('firsttheme-admin-scripts', get_template_directory_uri() . '/dist/asset/js/admin.js', array(), false);
+
 }
 
 add_action('admin_enqueue_scripts', 'firsttheme_admin_assets', 10);
