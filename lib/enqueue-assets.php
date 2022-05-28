836b7ea4ce36caeb601e58c5644d83224ddb81b8
Ahmad Raza <raza.ataki@gmail.com>
2022-05-28 19:57:53 +0500

    update files version

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -2,15 +2,15 @@
 
 function firsttheme_assets()
 {
-    wp_enqueue_style('firsttheme-stylesheet', get_template_directory_uri() . '/dist/asset/css/bundle.css', array(), 'all');
-    wp_enqueue_script('firsttheme-scripts', get_template_directory_uri() . '/dist/asset/js/bundle.js', array(), false);
+    wp_enqueue_style('firsttheme-stylesheet', get_template_directory_uri() . '/dist/asset/css/bundle.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('firsttheme-scripts', get_template_directory_uri() . '/dist/asset/js/bundle.js', array(), '1.0.0', false);
 }
 add_action('wp_enqueue_scripts', 'firsttheme_assets', 10);
 
 function firsttheme_admin_assets()
 {
-    wp_enqueue_style('firsttheme-admin-stylesheet', get_template_directory_uri() . '/dist/asset/css/admin.css', array(), 'all');
-    wp_enqueue_script('firsttheme-admin-scripts', get_template_directory_uri() . '/dist/asset/js/admin.js', array(), false);
+    wp_enqueue_style('firsttheme-admin-stylesheet', get_template_directory_uri() . '/dist/asset/css/admin.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('firsttheme-admin-scripts', get_template_directory_uri() . '/dist/asset/js/admin.js', array(), '1.0.0', false);
 
 }
 
