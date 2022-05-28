cb0f153639013653ae1ab03953a96f4ef821ee4b
Ahmad Raza <raza.ataki@gmail.com>
2022-05-28 14:58:11 +0500

    enqueue wp admin style

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -5,3 +5,10 @@ function firsttheme_assets()
     wp_enqueue_style('firsttheme-stylesheet', get_template_directory_uri() . '/dist/asset/css/bundle.css', array(), 'all');
 }
 add_action('wp_enqueue_scripts', 'firsttheme_assets', 10);
+
+function firsttheme_admin_assets()
+{
+    wp_enqueue_style('firsttheme-admin-stylesheet', get_template_directory_uri() . '/dist/asset/css/admin.css', array(), 'all');
+}
+
+add_action('admin_enqueue_scripts', 'firsttheme_admin_assets', 10);
