0d031920931423d5553f8ac970a50d25e3078e4d
Ahmad Raza <raza.ataki@gmail.com>
2022-05-28 14:46:41 +0500

    user gulp-sourcemaps to load main scss file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,12 +8,16 @@ import cleanCss from "gulp-clean-css";
 
 import gulpIf from "gulp-if";
 
+import sourcemaps from "gulp-sourcemaps";
+
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = (done) => {
   return gulp
     .src("./src/assets/scss/bundle.scss")
+    .pipe(gulpIf(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on("error", sass.logError))
     .pipe(gulpIf(PRODUCTION, cleanCss({ compatibility: "ie8" })))
+    .pipe(gulpIf(!PRODUCTION, sourcemaps.write()))
     .pipe(gulp.dest("dist/asset/css"));
 };
