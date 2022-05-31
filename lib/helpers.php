ef9e30ffbcc35cdd31b36a78ed5542aa5e582404
Ahmad Raza <raza.ataki@gmail.com>
2022-05-31 13:19:18 +0500

    creating child theme

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,18 +1,19 @@
 <?php
-function _themename_post_meta()
-{
-    printf(
-        esc_html__('Posted on %s ', '_themename'),
-        '<a href="' . esc_url(get_permalink()) . '"><time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date('l, F j, Y')) . '</time></a>'
-    );
+if (!function_exists('_themename_post_meta')) {
+    function _themename_post_meta()
+    {
+        printf(
+            esc_html__('Posted on %s ', '_themename'),
+            '<a href="' . esc_url(get_permalink()) . '"><time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date('l, F j, Y')) . '</time></a>'
+        );
 
-    printf(
-        esc_html__('By %s ', '_themename'),
-        '<a href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . "</a>"
-    );
+        printf(
+            esc_html__('By %s ', '_themename'),
+            '<a href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . "</a>"
+        );
 
+    }
 }
-
 function _themename_read_more_link()
 {
     echo '<a href="' . esc_url(get_the_permalink()) . '" title="' . esc_attr(the_title_attribute(['echo' => false])) . '">';
