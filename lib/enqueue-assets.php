e89967421b2dbe2c7fa6ecf1763d8ad431cc914e
Ahmad Raza <raza.ataki@gmail.com>
2022-06-01 16:37:32 +0500

    styling & filtering the navigation menu

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -3,7 +3,7 @@
 function _themename_assets()
 {
     wp_enqueue_style('_themename-stylesheet', get_template_directory_uri() . '/dist/asset/css/bundle.css', array(), '1.0.0', 'all');
-    wp_enqueue_script('_themename-scripts', get_template_directory_uri() . '/dist/asset/js/bundle.js', array('jquery'), '1.0.0', false);
+    wp_enqueue_script('_themename-scripts', get_template_directory_uri() . '/dist/asset/js/bundle.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', '_themename_assets', 10);
 
