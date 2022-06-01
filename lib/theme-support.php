b4c297057ab0c26912f1c00f375b8acb46c4d2ee
Ahmad Raza <raza.ataki@gmail.com>
2022-06-01 14:06:30 +0500

    add menu locations

--- lib/theme-support.php
+++ lib/theme-support.php
@@ -3,8 +3,10 @@
 function _themename_theme_support()
 {
     add_theme_support('title-tag');
+    add_theme_support('menus');
     add_theme_support('post-thumbnails');
     add_theme_support('html5', array('search-form', 'comment-list', 'comment-form', 'gallery', 'caption'));
+
 }
 
 add_action('after_setup_theme', '_themename_theme_support');
