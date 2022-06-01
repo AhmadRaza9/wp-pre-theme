6d80a0efd19a4dba09edee08d2551bfbb53141c8
Ahmad Raza <raza.ataki@gmail.com>
2022-06-01 10:42:01 +0500

    create 4 columns for footer widget using loop

--- footer.php
+++ footer.php
@@ -1,7 +1,21 @@
 </div>
 <!-- Container Ends Here -->
-<footer class="footer">
-    <h1>I am from Footer</h1>
+
+<?php
+$footer_layout = '3,3,3,3';
+$columns = explode(',', $footer_layout);
+$footer_bg = 'dark';
+?>
+
+<footer class="footer c-footer c-footer--<?php echo $footer_bg; ?>">
+    <div class="o-container">
+        <div class="o-row o-row-4">
+            <?php foreach ($columns as $i => $column) {?>
+                <div class="o-row__column o-row__column--span-12 o-row__column--span-<?php echo $column; ?>@medium"></div>
+            <?php }?>
+        </div>
+    </div>
 </footer>
+<?php wp_footer()?>
 </body>
 </html>
