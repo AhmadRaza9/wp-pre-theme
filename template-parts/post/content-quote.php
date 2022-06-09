be32cb06bdfea3fee71c8fbbc65ff2fec807626f
Ahmad Raza <raza.ataki@gmail.com>
2022-06-09 20:56:56 +0500

    add quote post format

---
+++ template-parts/post/content-quote.php
@@ -0,0 +1,13 @@
+<article <?php post_class("c-post u-margin-bottom-20");?>>
+    <div class="c-post__inner">
+            <div class="content c-post__excerpt">
+                <?php the_content();?>
+            </div>
+                <div class="meta-info c-post__meta">
+                    <?php _themename_post_meta();?>
+                </div>
+        <?php if (!is_single()): ?>
+            <?php get_template_part('template-parts/post/footer');?>
+        <?php endif;?>
+     </div>
+</article>
