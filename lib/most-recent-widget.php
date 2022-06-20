d27a4bb8d33c7c9818ad8c69890b40cd7666bc43
Ahmad Raza <raza.ataki@gmail.com>
2022-06-20 12:35:25 +0500

    Creating our first field in the widget'

--- lib/most-recent-widget.php
+++ lib/most-recent-widget.php
@@ -15,18 +15,29 @@ class _themename_Most_Recent_Widget extends WP_Widget
 
     public function form($instance)
     {
-        echo "test";
-    }
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        } else {
+            $title = 'Nothing!';
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php esc_html_e('Title', '_themename');?></label>
+            <input type="text" class="widefate" id="<?php echo $this->get_field_id('title'); ?>" value="<?php echo esc_attr($title); ?>" name="<?php echo $this->get_field_name('title'); ?>">
+        </p>
+        <?php
+}
 
     public function widget($args, $instance)
     {
-        var_dump($args);
-        echo "best";
+        echo "<h2>" . $instance['title'] . "</h2>";
     }
 
     public function update($new_instance, $old_instance)
     {
-
+        $instance = array();
+        $instance['title'] = isset($new_instance['title']) ? sanitize_text_field($new_instance['title']) : '';
+        return $instance;
     }
 }
 
